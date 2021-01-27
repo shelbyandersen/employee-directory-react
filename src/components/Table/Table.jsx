b3ea9d9a6f685ee5d902c9528b6e6f8d27e26027
Shelby <shelbyrobison14@gmail.com>
2021-01-26 23:28:19 -0500

    employees are sorted onClick in the console

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -12,22 +12,36 @@ useEffect(() => {
     })
 }, []);
 
+const sortByName = () => {
+    const sortedUsers = users.sort((a, b) => {
+        const aValue = a.name.first;
+        const bValue = b.name.first;
+    if (aValue < bValue) {
+        return -1;
+    }
+    if (aValue > bValue) {
+        return 1;
+    }
+    return 0;
+});
+console.log(sortedUsers);
+};
     return (
         <table className="table table-success table-striped">
   <thead>
     <tr>
       <th scope="col">ID</th>
       <th scope="col">Photo</th>
-      <th scope="col">Name</th>
+      <th scope="col" onClick={sortByName}>Name</th>
       <th scope="col">Email</th>
       <th scope="col">Phone</th>
     </tr>
   </thead>
   <tbody>
         {users.map((user) => (
-                <tr>
+                <tr key={user.id.value}>
                 <th scope="row">{user.id.value}</th>
-                <td><img src={user.picture.thumbnail}></img></td>
+                <td><img src={user.picture.thumbnail} alt={user.name.first}></img></td>
                 <td>{user.name.first} {user.name.last}</td>
                 <td>{user.email}</td>
                 <td>{user.phone}</td>
