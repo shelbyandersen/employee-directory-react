2d20916d76bf55219616c9fa68fc43dac7f27874
Shelby <shelbyrobison14@gmail.com>
2021-01-26 23:44:40 -0500

    users are sorted in ascending order when user clicks name

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,18 +2,20 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
 const Table = () => {
-
-const [users, setUsers] = useState([])
+const [users, setUsers] = useState([]);
+const [usersToDisplay, setUsersToDisplay] = useState([]);
 
 useEffect(() => {
     axios.get("https://randomuser.me/api/?results=50").then(response => {
         console.log(response.data);
+        setUsersToDisplay(response.data.results);
         setUsers(response.data.results);
-    })
+    });
 }, []);
 
 const sortByName = () => {
-    const sortedUsers = users.sort((a, b) => {
+    const tempUsers = [...users];
+    const sortedUsers = tempUsers.sort((a, b) => {
         const aValue = a.name.first;
         const bValue = b.name.first;
     if (aValue < bValue) {
@@ -25,6 +27,7 @@ const sortByName = () => {
     return 0;
 });
 console.log(sortedUsers);
+setUsersToDisplay(sortedUsers);
 };
     return (
         <table className="table table-success table-striped">
@@ -38,8 +41,8 @@ console.log(sortedUsers);
     </tr>
   </thead>
   <tbody>
-        {users.map((user) => (
-                <tr key={user.id.value}>
+        {usersToDisplay.map((user, index) => (
+                <tr key={index}>
                 <th scope="row">{user.id.value}</th>
                 <td><img src={user.picture.thumbnail} alt={user.name.first}></img></td>
                 <td>{user.name.first} {user.name.last}</td>
