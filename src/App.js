16df65c3be7787b880b26b89cec364e82683ef6d
Shelby <shelbyrobison14@gmail.com>
2021-01-27 22:17:46 -0500

    react build is updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { HashRouter as Route} from "react-router-dom";
-import Table from "./components/Table/Table"
+import { HashRouter as Router } from "react-router-dom";
+import Table from "./components/Table/Table";
 
 function App() {
   return (
-    <Router basename="/">    <div className="container">
-      <div className="row">
-        <div className="col">
-          <div className="App">
-            <Table />
+    <Router basename="/">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="App">
+              <Table />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </Router>
-
   );
 }
 
